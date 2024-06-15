3a1e5d275226b9c513727f689332e3a93779af42
nsfge <nsfgeo@hotmail.com>
2024-06-15 17:27:41 -0300

    feat: criado course service e instalado angular material.

---
+++ src/app/shared/app-material/app-material.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule
+  ]
+})
+export class AppMaterialModule { }
